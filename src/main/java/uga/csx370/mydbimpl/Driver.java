aedd29f9d8bfac1685097cc7270b2a94b7788c70
Seohyun Sasha Park <sp98996@uga.edu>
2026-09-17 22:56:35 -0400

    Add Sasha relational algebra query to Driver
    Added a three-table query using student, takes, and course to find students who earned an A in the Computer Science course "International Practicum".

--- src/main/java/uga/csx370/mydbimpl/Driver.java
+++ src/main/java/uga/csx370/mydbimpl/Driver.java
@@ -12,6 +12,11 @@ import uga.csx370.mydb.Relation;
 import uga.csx370.mydb.RelationBuilder;
 import uga.csx370.mydb.Type;
 
+// Sasha import
+import uga.csx370.mydb.Cell;
+import uga.csx370.mydb.Predicate;
+import uga.csx370.mydb.RA;
+
 public class Driver {
     
     public static void main(String[] args) {
@@ -29,12 +34,43 @@ public class Driver {
         // rel1.loadData("/path/to/exported/csv_file");
         // rel1.print();
 
+        // Sasha Add
+        RA ra = new RAImpl();
+        Relation students = new RelationBuilder()
+            .attributeNames(List.of("ID", "name", "dept_name", "tot_cred"))
+            .attributeTypes(List.of(
+                Type.INTEGER, Type.STRING, Type.STRING, Type.INTEGER))
+            .build();
+        
+        students.loadData("test-tables/student.csv");
+        
+        Relation takes = new RelationBuilder()
+            .attributeNames(List.of(
+                "ID", "course_id", "sec_id", "semester", "year", "grade"))
+            .attributeTypes(List.of(
+                Type.INTEGER, Type.STRING, Type.STRING,
+                Type.STRING, Type.INTEGER, Type.STRING))
+            .build();
+        
+        takes.loadData("test-tables/takes.csv");
+        
+        
+        Relation courses = new RelationBuilder()
+            .attributeNames(List.of(
+                "course_id", "title", "dept_name", "credits"))
+            .attributeTypes(List.of(
+                Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
+            .build();
+        
+        courses.loadData("test-tables/course.csv");
+
+        // Original
         Relation instructors = new RelationBuilder()
             .attributeNames(List.of("ID", "name", "dept_name", "salary"))
             .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
             .build();
         instructors.loadData("test-tables/instructor.csv");
-        instructors.print();
+        // instructors.print();
 
         // Relation students = new RelationBuilder()
         //     .attributeNames(List.of("ID", "name", "dept_name", "tot_cred"))
@@ -43,6 +79,59 @@ public class Driver {
         // students.loadData("test-tables/student.csv");
         // System.out.println("\nMyID: jcm68203 : 811281498");
         // students.print(); // warning: a lot of data will be printed to the console
+
+        // Sasha's Query
+        System.out.println(
+            "\nQuery: Find the names of students who earned an A " +
+            "in the Computer Science course \"International Practicum\"."
+        );
+        Relation renamedStudents = ra.rename(
+            students,
+            List.of("dept_name"),
+            List.of("student_dept")
+        );
+        Relation studentTakes = ra.join(renamedStudents, takes);
+        Relation studentTakesCourses = ra.join(studentTakes, courses);
+
+        Predicate csPredicate = new PredicateImpl(
+            10,
+            "=",
+            Cell.val("Comp. Sci.")
+        );
+        
+        Relation csCourses = ra.select(
+            studentTakesCourses,
+            csPredicate
+        );
+        
+        Predicate coursePredicate = new PredicateImpl(
+            9,
+            "=",
+            Cell.val("International Practicum")
+        );
+        
+        Relation practicum = ra.select(
+            csCourses,
+            coursePredicate
+        );
+        
+        Predicate gradePredicate = new PredicateImpl(
+            8,
+            "=",
+            Cell.val("A")
+        );
+        
+        Relation gradeA = ra.select(
+            practicum,
+            gradePredicate
+        );
+        
+        Relation result = ra.project(
+            gradeA,
+            List.of("name")
+        );
+        
+        result.print();
     }
 
 }
